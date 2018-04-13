dc8230314b641cbdcfc97af5c78dc8d621577069
Virginia <vewq48>
2018-04-12 19:45:58 -0500

    js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //Loads when the javascript has loaded. when menu-trigger is clicked the function slide toggle occurs. THe 500 is the length of the animation.
 
 $(document).ready(function() {
-  
+  $('.menu-trigger').click(function() {
+    $('nav ul').slideToggle(500);
+  });
