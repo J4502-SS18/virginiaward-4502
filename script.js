dbbf139552ac6432863f8e94d03b07438ef12c9b
Virginia <vewq48>
2018-04-13 16:00:53 -0500

    attempts at fixing nav

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-//Loads when the javascript has loaded. when menu-trigger is clicked the function slide toggle occurs. THe 500 is the length of the animation.
-
 $(document).ready(function() {
-  $('.menu-trigger').click(function() {
-    $('nav ul').slideToggle(500);
+    $('.menu-trigger').click(function() {
+      $('nav ul').slideToggle(500);
+    });
+
+    $(window).resize(function() {
+  	   if (  $(window).width() > 700 ) {
+  		$('nav ul').removeAttr('style');
+  	 }
+  	});
   });
